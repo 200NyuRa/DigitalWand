ae698b2c16496ea271c9acbfc18dbcc3afe52d88
Anna.Karyakina <karyakina.ann@gmail.com>
2016-09-09 15:31:08 +0300

    slider final

--- src/js/my_scripts/promo-slider.js
+++ src/js/my_scripts/promo-slider.js
@@ -107,7 +107,7 @@ var promoSlider = (function() {
 			timer = setTimeout(function play() {
 				$('.slider-promo__arrow--right').trigger('click');
 				timerId = setTimeout(play, 3000);
-			}, 0);
+			}, 3000);
 		},
 
 		clearTimer: function(){
